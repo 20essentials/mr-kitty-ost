95f36904947d44decea81f7056f4c3a236c5a56c
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:48:37 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MR. KITTY';
+export let nameSongs = [
+  'AFTER DARK' /*1*/,
+  'DEVOUR' /*2*/,
+  'DESTROY ME' /*3*/,
+  'DREAM DIVER' /*4*/,
+  'HEALING WATERS' /*5*/,
+  'HOLLOW' /*6*/,
+  'FLOWERS FOR BOYS' /*7*/,
+  'HABIT' /*8*/,
+  'LACERATIONS' /*9*/,
+  'IN YOUR ARMS TONIGHT' /*10*/,
+  'SPIRIT OF THE NIGHT' /*11*/,
+  'NO HEART' /*12*/,
+  'SCARS' /*13*/,
+  'PATHOGEN' /*14*/,
+  'HELL' /*15*/,
+  'XIII' /*16*/,
+  'TRAUMA' /*17*/,
+  'MELTING CORE' /*18*/,
+  'BLEED BLACK' /*19*/,
+  'ANGELS' /*20*/
+];
